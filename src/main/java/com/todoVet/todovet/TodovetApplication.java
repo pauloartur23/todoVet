ca8ca8199bf1b0bf3873f5373a7045d55473236d
pauloartur23 <pauloarturrg123@gmail.com>
2026-09-07 16:31:48 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ src/main/java/com/todoVet/todovet/TodovetApplication.java
@@ -0,0 +1,13 @@
+package com.todoVet.todovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodovetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodovetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoVet/todovet/TodovetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoVet.todovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodovetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
