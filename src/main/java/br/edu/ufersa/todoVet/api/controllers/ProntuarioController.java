594e8aa875641a6d52ef29f0af9e4e87eb7b5532
davi <kikifelipecostabenevides@gmail.com>
2026-09-10 20:37:11 -0300

    feat: estrutura inicial dos controllers acerca dos prontuarios

---
+++ src/main/java/br/edu/ufersa/todoVet/api/controllers/ProntuarioController.java
@@ -0,0 +1,41 @@
+package br.edu.ufersa.todoVet.api.controllers;
+
+import br.edu.ufersa.todoVet.api.dtos.prontuario.*;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pets/{petId}/prontuarios")
+public class ProntuarioController {
+
+    @GetMapping
+    public ResponseEntity<List<ProntuarioResponse>> listarPorPet(@PathVariable Long petId) {
+        return null;
+    }
+
+    @GetMapping("/{prontuarioId}")
+    public ResponseEntity<ProntuarioResponse> buscarPorId(
+            @PathVariable Long petId,
+            @PathVariable Long prontuarioId) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProntuarioResponse> registrar(
+            @PathVariable Long petId,
+            @RequestBody @Valid ProntuarioCreate dto,
+            UriComponentsBuilder uriBuilder) {
+
+        URI uri = uriBuilder
+                .path("/api/v1/pets/{petId}/prontuarios/{prontuarioId}")
+                .buildAndExpand(petId, 1L)
+                .toUri();
+
+        return ResponseEntity.created(uri).body(null);
+    }
+}
