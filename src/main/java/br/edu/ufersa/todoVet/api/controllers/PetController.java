7eb2fdd31f6dd2677c7512faeef92981a78df037
diogogildecio <diogogildecio19@gmail.com>
2026-09-11 20:21:11 -0300

    Criando PetController

--- src/main/java/br/edu/ufersa/todoVet/api/controllers/PetController.java
+++ src/main/java/br/edu/ufersa/todoVet/api/controllers/PetController.java
@@ -1,5 +1,38 @@
 package br.edu.ufersa.todoVet.api.controllers;
 
+import br.edu.ufersa.todoVet.api.dtos.PetRequestDTO;
+import br.edu.ufersa.todoVet.api.dtos.PetResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pets")
 public class PetController {
 
+    @GetMapping
+    public ResponseEntity<List<PetResponseDTO>> listarTodos() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PetResponseDTO> buscarPorId(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<PetResponseDTO> criar(@RequestBody @Valid PetRequestDTO novoPet) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PetResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid PetRequestDTO petAtualizado) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        return null;
+    }
 }
