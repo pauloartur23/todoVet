c3634b1ba84fbabb79ce9a85a4fc2e425ce94989
rianvvsp11 <rian333sx@exemplo.com>
2026-09-12 19:47:42 -0300

    adição de controller de usuario, e dto

---
+++ src/main/java/br/edu/ufersa/todoVet/api/controllers/UsuarioController.java
@@ -0,0 +1,39 @@
+package br.edu.ufersa.todoVet.api.controllers;
+
+import br.edu.ufersa.todoVet.api.dtos.UsuarioRequestDTO;
+import br.edu.ufersa.todoVet.api.dtos.UsuarioResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+public class UsuarioController {
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarTodos() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid UsuarioRequestDTO usuarioAtualizado) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<UsuarioResponseDTO>> buscarPorNome(@RequestParam String nome) {
+        return null;
+    }
+}
---
+++ src/main/java/br/edu/ufersa/todoVet/api/dtos/UsuarioRequesteDTO.java
@@ -0,0 +1,17 @@
+package br.edu.ufersa.todoVet.api.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UsuarioRequestDTO(
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "Telefone é obrigatório")
+        String telefone
+) {
+}
