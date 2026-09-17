632242346e24255b36b8d2d3c64016f6b8821580
diogogildecio <diogogildecio19@gmail.com>
2026-09-17 16:46:30 -0300

    Alteração de detalhe

--- src/main/java/br/edu/ufersa/todoVet/api/controllers/FuncionarioController.java
+++ src/main/java/br/edu/ufersa/todoVet/api/controllers/FuncionarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/usuarios")
+@RequestMapping("/api/v1/funcionarios")
 public class FuncionarioController {
 
     @GetMapping
